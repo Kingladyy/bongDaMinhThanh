20858d919ae5f04218bbf8b6b6c9558298fab7f1
minh <lamkool2110@gmail.com>
2025-05-12 08:38:59 +0700

    dc roi

--- js/main.js
+++ js/main.js
@@ -121,3 +121,18 @@ document.addEventListener('DOMContentLoaded', function () {
       }
     });
   });
+
+let lastScroll = 0;
+const navBar = document.querySelector('nav');
+
+window.addEventListener('scroll', function() {
+  const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+  if (currentScroll > lastScroll && currentScroll > 60) {
+    // Kéo xuống
+    navBar.classList.add('hide');
+  } else {
+    // Kéo lên
+    navBar.classList.remove('hide');
+  }
+  lastScroll = currentScroll <= 0 ? 0 : currentScroll;
+});
